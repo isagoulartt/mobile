11e7a6479d4a89e50c70964827c680af7fff55b0
ISABELY AMABILE GOULART <isabely_goulart@estudante.sesisenai.org.br>
2024-09-12 12:20:28 -0300

    quinta

---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { View, StyleSheet, Text} from "react-native";
+import {Link} from 'expo-router';
+
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    box:{
+        shadowOpacity: 1.25,  
+        shadowRadius: 10,
+        borderRadius: 10, 
+       padding: 50
+    },
+
+    texto:{
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+        fontFamily:'courier new',
+        fontWeight:'800',
+    }
+   
+  });
+
+export default function Page(){
+    return(
+        <View style={style.container}> 
+           <View style={style.box}>
+           
+                <Text style={style.texto}>Atividades</Text>
+            <Link href= "/login">Faça seu login</Link>
+            <Link href= "/banco">App do banco</Link>
+            <Link href= "/(tabs)\calculadora">Calculadora</Link>
+            <Link href= "/spotify">Entre no spotify</Link>
+            <Link href= "/lista">Lista de músicas</Link>
+            <Link href= "/pokemon">Escolha seu pokemon</Link>
+            </View>
+    
+        </View>
+    );
+}
+
